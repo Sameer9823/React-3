3cce0af78cb7e06a44ce4497ebe814c49bd9f465
Sameer9823 <sameerselokar924@gmail.com>
2024-02-16 21:25:13 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const[x, setX] = useState(0);
+ 
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+    <div className='text1'>
+
+    Count:{x} 
+
     </div>
-  );
+
+     
+     <button className='btn' onClick={() => setX(x-1)}>Increment</button>
+     <button className="btn"
+     onClick={() => setX(x+1)}>Decrement</button>
+    </>
+  )
 }
 
 export default App;
